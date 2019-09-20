940ab1a5cb7cc150e6de6027a93b0e7647a415e7
Nick McIntyre <nick@mcintyre.io>
2019-09-19 19:57:49 -0500

    Tensor add, sub, equals

--- src/tensor.ts
+++ src/tensor.ts
@@ -1,6 +1,6 @@
 import * as tfc from '@tensorflow/tfjs-core';
 
-import p5 from "./index";
+import p5 from './index';
 
 
 /**
@@ -10,18 +10,71 @@ import p5 from "./index";
  * engineering, and computer science.
  */
 class Tensor {
-  private tensor: tfc.Tensor;
+  public tensor: tfc.Variable;
 
   constructor(obj: any) {
-    if (typeof obj === 'number') {
-      this.tensor = tfc.scalar(obj);
-    } else if (obj instanceof Array) {
-      this.tensor = tfc.tensor(obj);
-    } else if (obj instanceof p5.Vector) {
-      this.tensor = tfc.tensor(obj.array());
-    } else {
-      throw new Error('Tensors must be created from Numbers, Arrays, or p5.Vectors.');
-    }
+    tfc.tidy(() => {
+      if (typeof obj === 'number') {
+        this.tensor = tfc.variable(tfc.scalar(obj));
+      } else if (obj instanceof Array) {
+        this.tensor = tfc.variable(tfc.tensor(obj));
+      } else if (obj instanceof p5.Vector) {
+        this.tensor = tfc.variable(tfc.tensor(obj.array()));
+      } else {
+        throw new Error('Tensors must be created from Numbers, Arrays, or p5.Vectors.');
+      }
+    });
+  }
+
+  /**
+   * Equality check against a Tensor.
+   * 
+   * @param b the tensor to be compared
+   */
+  equals(b: Tensor): boolean {
+    let result = false;
+    tfc.tidy(() => {
+      const check = tfc.all(this.tensor.equal(b.tensor));
+      if (check.arraySync() === 1) {
+        result = true;
+      };
+    });
+
+    return result;
+  }
+
+  /**
+   * Adds two tensors.
+   * 
+   * @param b the tensor to be added
+   */
+  add(b: Tensor) {
+    tfc.tidy(() => {
+      const result = this.tensor.add(b.tensor);
+      if (this.tensor.rank < result.rank) {
+        this.tensor.dispose();
+        this.tensor = tfc.variable(result);
+      } else {
+        this.tensor.assign(result);
+      }
+    });
+  }
+
+  /**
+   * Subtracts two tensors.
+   * 
+   * @param b the tensor to be subtracted
+   */
+  sub(b: Tensor) {
+    tfc.tidy(() => {
+      const result = this.tensor.sub(b.tensor);
+      if (this.tensor.rank < result.rank) {
+        this.tensor.dispose();
+        this.tensor = tfc.variable(result);
+      } else {
+        this.tensor.assign(result);
+      }
+    });
   }
 };
 
--- test/unit/tensor.js
+++ test/unit/tensor.js
@@ -35,5 +35,89 @@ describe('Tensor', function () {
 
       expect(t).to.be.an.instanceof(pInst.Tensor);
     });
+
+    it('Should reject garbage arguments', function () {
+      expect(() => pInst.createTensor("I'm only happy when it rains")).to.throw(Error);
+    });
+  });
+
+  describe('equals()', function () {
+    it('Should require a Tensor as an argument', function () {
+      const t = pInst.createTensor(1);
+
+      expect(() => t.equals(1)).to.throw(Error);
+    });
+
+    it('Should return true when logically equal', function () {
+      const t1 = pInst.createTensor([0, 1]);
+      const t2 = pInst.createTensor([0, 1]);
+
+      expect(t1.equals(t2)).to.equal(true);
+    });
+
+    it('Should return false when not logically equal', function () {
+      const t1 = pInst.createTensor([0, 1]);
+      const t2 = pInst.createTensor([1, 0]);
+
+      expect(t1.equals(t2)).to.equal(false);
+    });
+  });
+
+  describe('add()', function () {
+    it('Should add in place', function () {
+      const t1 = pInst.createTensor([1, 2]);
+      const t2 = pInst.createTensor([3, 4]);
+      const t3 = pInst.createTensor([4, 6]);
+      t1.add(t2);
+
+      expect(t1.equals(t3)).to.equal(true);
+    });
+
+    it('Should broadcast addition when maintaining rank', function () {
+      const t1 = pInst.createTensor([1, 2]);
+      const t2 = pInst.createTensor(1);
+      const t3 = pInst.createTensor([2, 3]);
+      t1.add(t2);
+
+      expect(t1.equals(t3)).to.equal(true);
+    });
+
+    it('Should broadcast addition when increasing rank', function () {
+      const t1 = pInst.createTensor(1);
+      const t2 = pInst.createTensor([1, 2]);
+      const t3 = pInst.createTensor([2, 3]);
+      t1.add(t2);
+
+      expect(t1.equals(t3)).to.equal(true);
+    });
+  });
+
+  describe('sub()', function () {
+    it('Should subtract in place', function () {
+      const t1 = pInst.createTensor([3, 4]);
+      const t2 = pInst.createTensor([1, 2]);
+      const t3 = pInst.createTensor([2, 2]);
+      t1.sub(t2);
+
+      expect(t1.equals(t3)).to.equal(true);
+    });
+
+    it('Should broadcast subtraction when maintaining rank', function () {
+      const t1 = pInst.createTensor([2, 3]);
+      const t2 = pInst.createTensor(1);
+      const t3 = pInst.createTensor([1, 2]);
+      t1.sub(t2);
+
+      expect(t1.equals(t3)).to.equal(true);
+    });
+
+    it('Should broadcast subtraction when increasing rank', function () {
+      const t1 = pInst.createTensor(1);
+      const t2 = pInst.createTensor([2, 3]);
+      const t3 = pInst.createTensor([-1, -2]);
+      t1.sub(t2);
+
+      expect(t1.equals(t3)).to.equal(true);
+    });
   });
 });
