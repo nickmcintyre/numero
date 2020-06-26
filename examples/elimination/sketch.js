5c67ffc59d720afab3ea1fc0c6db339374f4be9a
Nick McIntyre <nick@mcintyre.io>
2020-06-26 14:13:17 -0500

    Update elimination example

--- examples/elimination/sketch.js
+++ examples/elimination/sketch.js
@@ -8,23 +8,33 @@ function setup() {
   let a = createTensor([[ 2,  1, -1,   8],
                         [-3, -1,  2, -11],
                         [-2,  1,  2,  -3]]);
+  print('Original Matrix');
   print(a.toString());
   a = a.addRows(0, 1, 1.5);
+  print('Row 1 + 1.5 * Row 0');
   print(a.toString());
   a = a.addRows(0, 2);
+  print('Row 2 + Row 0');
   print(a.toString());
   a = a.subRows(1, 2, 4);
+  print('Row 2 - 4 * Row 1');;
   print(a.toString());
   a = a.addRows(2, 1, 0.5);
+  print('Row 1 + 0.5 * Row 2');
   print(a.toString());
   a = a.subRows(2, 0);
+  print('Row 0 - Row 2');
   print(a.toString());
   a = a.mulRow(1, 2);
+  print('Row 1 -> 2 * Row 1');
   print(a.toString());
   a = a.mulRow(2, -1);
+  print('Row 2 -> -1 * Row 2');
   print(a.toString());
   a = a.subRows(1, 0);
+  print('Row 0 - Row 1');
   print(a.toString());
   a = a.mulRow(0, 0.5);
+  print('Row 0 -> 0.5 * Row 0');
   print(a.toString());
 }
